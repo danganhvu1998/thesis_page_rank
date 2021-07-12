910ba6458d5f7f1a20f83c785298037560b2e6c6
Anh Vu <danganhvu1998@gmail.com>
2021-07-12 10:45:14 +0900

    run data 10e6

--- cpp/page_rank_with_redis/pageRank.cpp
+++ cpp/page_rank_with_redis/pageRank.cpp
@@ -120,7 +120,7 @@ void __report() {
 int main() {
     getRunningEnv(); debugLevel = 1;
     redisCommand(local, "FLUSHALL");
-    freopen("graph_10e5.out", "r", stdin);
+    freopen("graph_10e6.out", "r", stdin);
     // INPUT GRAPH
     cin >> N >> M;
     localWorkerEndNode = min(localWorkerEndNode, N);
