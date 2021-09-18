921feb00b231fd07aab32cced09fbd0169d3f1e1
Danny Dang <danganhvu1998@gmail.com>
2021-09-18 09:50:49 +0900

    edit the script

--- script/spartk-install.sh
+++ script/spartk-install.sh
@@ -1,8 +1,8 @@
 sudo apt install default-jdk scala git -y
 java -version; javac -version; scala -version; git --version
-wget https://downloads.apache.org/spark/spark-3.0.1/spark-3.0.1-bin-hadoop2.7.tgz
+wget https://downloads.apache.org/spark/spark-3.1.2/spark-3.1.2-bin-hadoop2.7.tgz
 tar xvf spark-*
-sudo mv spark-3.0.1-bin-hadoop2.7 /opt/spark
+sudo  mv spark-3.1.2-bin-hadoop2.7 /opt/spark
 echo "export SPARK_HOME=/opt/spark" >> ~/.profile
 echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.profile
 echo "export PYSPARK_PYTHON=/usr/bin/python3" >> ~/.profile
