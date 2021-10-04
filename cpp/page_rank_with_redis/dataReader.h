7877a92bc9d14fc6c3c991ab2212100107410021
Danny Dang <danganhvu1998@gmail.com>
2021-10-04 14:08:05 +0900

    fix log with wrong data

--- cpp/page_rank_with_redis/dataReader.h
+++ cpp/page_rank_with_redis/dataReader.h
@@ -126,7 +126,10 @@ void getAllNodesValue(long long roundId) {
                         usleep(1000000);
                     }
                 }
-                printf("getNodesValRedis: got %lld / %lld nodes from worker %s\n", j, workersList[i].endNode - workersList[i].startNode, workersList[i].ip);
+                printf("getNodesValRedis: got %lld / %lld nodes from worker %s\n",
+                    j - workersList[i].startNode,
+                    workersList[i].endNode - workersList[i].startNode,
+                    workersList[i].ip);
                 free(command);
                 next = j + 1;
             }
