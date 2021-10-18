8c4ada9bf20880679f058aabb4112be41e7a043f
danganhvu <danganhvu1998@gmail.com>
2021-10-18 14:27:28 +0900

    fix lack of valiable in env

--- cpp/page_rank_with_redis/envs/wca5.h
+++ cpp/page_rank_with_redis/envs/wca5.h
@@ -6,3 +6,4 @@ const double CLOCK_RATE_GHZ = 1.5;
 const char* LOCAL_IP_ADDRESS = "192.168.99.205";
 const char* MAIN_WORKER_IP_ADDRESS = "192.168.99.207";
 const int MAX_SIZE_BULK_GET = 100000;
+const float STORAGE_FOR_CACHING_IN_GB = 1.0;
