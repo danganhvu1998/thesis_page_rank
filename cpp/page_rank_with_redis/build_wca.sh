a08d635b97a840d43be09a53584fe7652fc524b5
danganhvu <danganhvu1998@gmail.com>
2021-10-18 14:29:07 +0900

    build all

--- cpp/page_rank_with_redis/build_wca.sh
+++ cpp/page_rank_with_redis/build_wca.sh
@@ -1,3 +1,27 @@
 rm env.h
 cp ./envs/wca1.h env.h
 g++ pageRank.cpp -o pagerank_wca1.out -lhiredis -fopenmp
+
+rm env.h
+cp ./envs/wca2.h env.h
+g++ pageRank.cpp -o pagerank_wca1.out -lhiredis -fopenmp
+
+rm env.h
+cp ./envs/wca3.h env.h
+g++ pageRank.cpp -o pagerank_wca1.out -lhiredis -fopenmp
+
+rm env.h
+cp ./envs/wca4.h env.h
+g++ pageRank.cpp -o pagerank_wca1.out -lhiredis -fopenmp
+
+rm env.h
+cp ./envs/wca5.h env.h
+g++ pageRank.cpp -o pagerank_wca1.out -lhiredis -fopenmp
+
+rm env.h
+cp ./envs/wca6.h env.h
+g++ pageRank.cpp -o pagerank_wca1.out -lhiredis -fopenmp
+
+rm env.h
+cp ./envs/wca7.h env.h
+g++ pageRank.cpp -o pagerank_wca1.out -lhiredis -fopenmp
