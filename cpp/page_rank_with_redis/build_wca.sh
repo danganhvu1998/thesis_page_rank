3753afa6f3aa564c77063079f9f3055d13d5f1f3
danganhvu <danganhvu1998@gmail.com>
2021-10-22 13:34:57 +0900

    update build command

--- cpp/page_rank_with_redis/build_wca.sh
+++ cpp/page_rank_with_redis/build_wca.sh
@@ -1,27 +1,27 @@
 rm env.h
 cp ./envs/wca1.h env.h
-g++ pageRank.cpp -o pagerank_wca1 -lhiredis
+g++ pageRank.cpp -o pagerank_wca1 -lhiredis -fopenmp
 
 rm env.h
 cp ./envs/wca2.h env.h
-g++ pageRank.cpp -o pagerank_wca2 -lhiredis
+g++ pageRank.cpp -o pagerank_wca2 -lhiredis -fopenmp
 
 rm env.h
 cp ./envs/wca3.h env.h
-g++ pageRank.cpp -o pagerank_wca3 -lhiredis
+g++ pageRank.cpp -o pagerank_wca3 -lhiredis -fopenmp
 
 rm env.h
 cp ./envs/wca4.h env.h
-g++ pageRank.cpp -o pagerank_wca4 -lhiredis
+g++ pageRank.cpp -o pagerank_wca4 -lhiredis -fopenmp
 
 rm env.h
 cp ./envs/wca5.h env.h
-g++ pageRank.cpp -o pagerank_wca5 -lhiredis
+g++ pageRank.cpp -o pagerank_wca5 -lhiredis -fopenmp
 
 rm env.h
 cp ./envs/wca6.h env.h
-g++ pageRank.cpp -o pagerank_wca6 -lhiredis
+g++ pageRank.cpp -o pagerank_wca6 -lhiredis -fopenmp
 
 rm env.h
 cp ./envs/wca7.h env.h
-g++ pageRank.cpp -o pagerank_wca7 -lhiredis
+g++ pageRank.cpp -o pagerank_wca7 -lhiredis -fopenmp
