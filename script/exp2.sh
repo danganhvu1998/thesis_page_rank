e5aadb311b4c8167a208e1c56bed697bd36440f0
Anh Vu <danganhvu1998@gmail.com>
2021-12-24 00:10:22 +0900

    test if graphX much slower if data set is larger than worker ram

---
+++ script/exp2.sh
@@ -0,0 +1,51 @@
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_160.out" --numEPart=-1 --numIter=1
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_160.out" --numEPart=-1 --numIter=2
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_160.out" --numEPart=-1 --numIter=4
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_160.out" --numEPart=-1 --numIter=8
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_160.out" --numEPart=-1 --numIter=12
+
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_320.out" --numEPart=-1 --numIter=1
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_320.out" --numEPart=-1 --numIter=2
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_320.out" --numEPart=-1 --numIter=4
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_320.out" --numEPart=-1 --numIter=8
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_320.out" --numEPart=-1 --numIter=12
+
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_640.out" --numEPart=-1 --numIter=1
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_640.out" --numEPart=-1 --numIter=2
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_640.out" --numEPart=-1 --numIter=4
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_640.out" --numEPart=-1 --numIter=8
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_640.out" --numEPart=-1 --numIter=12
+
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_1280.out" --numEPart=-1 --numIter=1
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_1280.out" --numEPart=-1 --numIter=2
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_1280.out" --numEPart=-1 --numIter=4
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_1280.out" --numEPart=-1 --numIter=8
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 1200m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_1280.out" --numEPart=-1 --numIter=12
+
+
+
+
+
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_80.out" --numEPart=-1 --numIter=1
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_80.out" --numEPart=-1 --numIter=2
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_80.out" --numEPart=-1 --numIter=4
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_80.out" --numEPart=-1 --numIter=8
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_80.out" --numEPart=-1 --numIter=12
+
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_160.out" --numEPart=-1 --numIter=1
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_160.out" --numEPart=-1 --numIter=2
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_160.out" --numEPart=-1 --numIter=4
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_160.out" --numEPart=-1 --numIter=8
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_160.out" --numEPart=-1 --numIter=12
+
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_320.out" --numEPart=-1 --numIter=1
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_320.out" --numEPart=-1 --numIter=2
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_320.out" --numEPart=-1 --numIter=4
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_320.out" --numEPart=-1 --numIter=8
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_320.out" --numEPart=-1 --numIter=12
+
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_640.out" --numEPart=-1 --numIter=1
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_640.out" --numEPart=-1 --numIter=2
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_640.out" --numEPart=-1 --numIter=4
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_640.out" --numEPart=-1 --numIter=8
+time ~/spark-3.1.2-bin-hadoop3.2/bin/run-example --master spark://wca-rpi4-7:7077 --executor-memory 600m graphx.Analytics pagerank "/home/wcauser3/thesis_page_rank/cpp/page_rank_with_redis/data/graph_100000_640.out" --numEPart=-1 --numIter=12
